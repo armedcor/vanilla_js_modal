01911a3125d68366d523d341191f5fd9b32607ac
Cormac Lawlor <armedcor@gmail.com>
2021-02-08 20:00:26 +0000

    Enable close modal functionality and refactor

--- script.js
+++ script.js
@@ -5,10 +5,26 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', function () {
-    console.log('Button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
+    openModal();
   });
 }
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', function () {
+  closeModal();
+});
+
+overlay.addEventListener('click', function () {
+  closeModal();
+});
